a48c77fb7fb699f1f5b18b56b3f62c723c1bc2f0
Damião Castro <damiao.c@hotmail.com>
2021-02-12 17:15:52 +0100

    cancel job

--- dataflow.js
+++ dataflow.js
@@ -9,33 +9,56 @@ async function launch() {
 
     const dataflow = google.dataflow({ version: 'v1b3', auth: auth });
 
-    const response = await dataflow.projects.locations.templates.launch({
-        projectId: "damiao-project-1",
-        gcsPath: "gs://dataflow-templates-europe-west6/latest/PubSub_Subscription_to_BigQuery",
-        validateOnly: false,
+    // const response = await dataflow.projects.locations.templates.launch({
+    //     projectId: "damiao-project-1",
+    //     gcsPath: "gs://dataflow-templates-europe-west6/latest/PubSub_Subscription_to_BigQuery",
+    //     validateOnly: false,
+    //     location: "europe-west1",
+    //     requestBody: {
+    //         jobName: "test_6",
+    //         environment: {
+    //             bypassTempDirValidation: false,
+    //             maxWorkers: 1,
+    //             machineType: "n1-standard-1",
+    //             tempLocation: "gs://damiao-project-1/temp",
+    //             ipConfiguration: "WORKER_IP_UNSPECIFIED",
+    //             enableStreamingEngine: true,
+    //             additionalExperiments: []
+    //         },
+    //         parameters: {
+    //             inputSubscription: "projects/damiao-project-1/subscriptions/test-subscription",
+    //             javascriptTextTransformGcsPath: "gs://damiao-project-1/udf.js",
+    //             javascriptTextTransformFunctionName: "transform",
+    //             outputTableSpec: "damiao-project-1:Business.dataflow_test",
+    //             outputDeadletterTable: "damiao-project-1:Business.dataflow_test_error_records"
+    //         }
+    //     }
+    // }, {});
+
+    const response = await dataflow.projects.locations.jobs.update({
         location: "europe-west1",
+        projectId: "damiao-project-1",
+        jobId: "2021-02-12_07_09_14-3724392685566488458",
         requestBody: {
-            jobName: "test_6",
-            environment: {
-                bypassTempDirValidation: false,
-                maxWorkers: 1,
-                machineType: "n1-standard-1",
-                tempLocation: "gs://damiao-project-1/temp",
-                ipConfiguration: "WORKER_IP_UNSPECIFIED",
-                enableStreamingEngine: true,
-                additionalExperiments: []                
-            },
-            parameters: {
-                inputSubscription: "projects/damiao-project-1/subscriptions/test-subscription",
-                javascriptTextTransformGcsPath: "gs://damiao-project-1/udf.js",
-                javascriptTextTransformFunctionName: "transform",
-                outputTableSpec: "damiao-project-1:Business.dataflow_test",
-                outputDeadletterTable: "damiao-project-1:Business.dataflow_test_error_records"
-            }
+            "id": "2021-02-12_07_09_14-3724392685566488458",
+            "projectId": "damiao-project-1",
+            requestedState: "JOB_STATE_CANCELLED"
         }
     }, {});
 
-    console.log(response);
+    console.log(JSON.stringify(response));
+
+    // const response = await
+    //     dataflow.projects.jobs.aggregated({
+    //         projectId: "damiao-project-1",
+    //         filter: "ACTIVE",
+    //         // location: "europe-west1",
+    //     });
+
+    // const job1 = response.data.jobs[0];
+
+
+    // console.log(JSON.stringify(job1n));
 }
 
 launch();
